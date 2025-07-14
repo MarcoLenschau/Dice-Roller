5d0c8b7f987ec9bb540e9570f7f53f4aabd0c9cf
MarcoLenschau <contact@marco-lenschau.de>
2025-07-14 03:19:35 +0200

    feat: Add jinja templates to app

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+def render(file, context = None, author = "Marco Lenschau"):
+    return render_template(file, _name=context, _author=author)
+
 @app.route('/')
-def home():
-    return "Hello, World!"
+@app.route('/<string:name>')
+def home(name = None):
+    return render("index.html", context=name)
+
+@app.route('/dices/')
+@app.route('/dices/<number>')
+def handle_dices(number=None):
+    return render('dices.html')
